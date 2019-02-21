08107c8b627e01c0e5595975f216b270d4d75635
Mark Raymond Jr <markrjr@live.com>
2019-02-21 01:36:19 -0600

    Initial commit.

---
+++ Assets/Scripts/SnowborgController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowborgController : MonoBehaviour {
+
+	private GameObject snowCore;
+	private Transform[] children;
+
+	// Use this for initialization
+	void Start () {
+
+		children = GetComponentsInChildren<Transform>();
+
+		foreach(Transform child in children)
+		{
+			if(child.gameObject.name == "Core")
+			{
+				snowCore = child.gameObject;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
